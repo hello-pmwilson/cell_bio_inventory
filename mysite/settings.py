cc139ce8c0f206cbae64c2e4d0d0ff938a6606c2
Paige Wilson <hello.pmwilson@gmail.com>
2024-01-13 12:33:37 -0500

    set up basics for local development

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+#Load env variables from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
