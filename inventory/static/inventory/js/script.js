cf5d3e5b07db816d7b2b500ac599c72c2ec421b1
Paige Wilson <hello.pmwilson@gmail.com>
2024-02-18 10:11:25 -0500

    add loadingflask animation

--- inventory/static/inventory/js/script.js
+++ inventory/static/inventory/js/script.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+    //Loading Animation - Loading Flask
+    //Thanks yqnn.github.io/svg-path-editor/ for the editor which was used to make the svg
+    var loadingFlaskPath = $("#loading-flask-path")
+    const paths = [
+        "m8 53q-10 0-1-12l6-6v-18q-5-4 0-4h7q5 0 0 4v18l6 6q10 12 0 12h-18m6-39q-3 0 0 2v19l-6 6q5-3 11 1 4 2 6-1l-6-6v-19q3-2 0-2M15 5A1 1 0 0013 7 1 1 0 0015 5M21-2A1 1 0 0021 1 1 1 0 0021-2M15-13A1 1 0 0015-10 1 1 0 0015-13",
+        "m8 53q-10 0-1-12l6-6v-18q-5-4 0-4h7q5 0 0 4v18l6 6q10 12 0 12h-18m6-39q-3 0 0 2v19l-6 6q1 5 9 1 6-2 8-1l-6-6v-19q3-2 0-2m2-8a1 1 0 002 2 1 1 0 00-2-2m-8-6a1 1 0 00-2 2 1 1 0 002-2m8-9a1 1 0 002 2 1 1 0 00-2-2"]
+    var i = 0;
+    function loopLoadingFlask() {
+        var d = i % 2 === 0 ? 0 : 1;
+        loadingFlaskPath.attr("d", paths[d]);
+        i++;
+        setTimeout(loopLoadingFlask, 500); // Wait half a second before calling the loop again
+        }
+    
+    // loopLoadingFlask(); // Start the loop
+    
+
     //as user types in the search bar, search through all cells in the table
     //skipping the form row, and update what is shown in the table based on matches
     //case insensitive
